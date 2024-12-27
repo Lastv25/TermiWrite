d8d2cf8f27f494b35528ea48fda4bde629622de1
Lastv25 <hg.mnjxgmail.com>
2024-12-27 10:32:53 +0100

    changing icons for files and folders

--- src/components/file_explorer.py
+++ src/components/file_explorer.py
@@ -2,5 +2,9 @@ from textual.widgets import DirectoryTree
 
 
 class FileExplorerDirTree(DirectoryTree):
+    ICON_NODE_EXPANDED = "▾ "
+    ICON_NODE = "▸ "
+    ICON_FILE = "⦁ "  # Remove icons in the result
+
     def __init__(self, folder_path: str) -> None:
         super().__init__(folder_path)
